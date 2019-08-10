19eb7811d5386e12638d9c6f38a6758ace70bec9
carlos <info@hechoenmargarita.com>
2019-08-10 16:30:12 +0200

    testing components en el proyecto Testc

---
+++ Modules/Modular.Modules.ModuleC/Components/MouleCViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modular.Modules.ModuleC.Components
+{
+    public class MoulecViewComponent : ViewComponent
+    {
+
+    }
+}
---
+++ Modules/Modular.Modules.ModuleC/ViewComponents/EmployeeList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modular.Modules.ModuleC.Components
+{
+    //[ViewComponent(Name = "Footer")]
+    [ViewComponent(Name = "EmployeeList1")]
+    public class MoulecViewComponent : ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //ExampleModel model = new ExampleModel(2);
+
+            // returns a fin
+
+            /return Task.FromResult<IViewComponentResult>(View(model));
+            return View();
+        }
+    }
+
+    internal class ExampleModel
+    {
+        private int v;
+
+        public ExampleModel(int v)
+        {
+            this.v = v;
+        }
+    }
+}
